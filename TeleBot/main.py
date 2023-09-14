3074577a4c4e158b6237978e7f4dd719e9fadaaa
hayrapetyan1998 <141331933+hayrapetyan1998@users.noreply.github.com>
2023-09-14 12:17:32 +0400

    Add files via upload

--- TeleBot/main.py
+++ TeleBot/main.py
@@ -1,6 +1,6 @@
 import telebot
 import sqlite3
-
+import csv
 
 bot = telebot.Telebot("6384674184:AAERz9OSmqyXMUYGzQD5TAfLEjKu08VxZc0")
 
@@ -13,3 +13,9 @@ def startfunc():
 
 
 bot.polling(none_stop=True)
+
+
+with open("store.csv", "r") as f:
+    content = csv.reader(f)
+    for item in content:
+        print(item)
